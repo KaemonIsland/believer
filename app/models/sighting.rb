5d657e82f9da308b156486bb13fa96bf2b388d54
Kaemon Lovendahl <klovendahl@deseretbook.com>
2020-10-11 16:05:53 -0600

    Add default for missing sighting data

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Sighting < ApplicationRecord
+  after_initialize :init
+
   validates :sighting_date, presence: true
   validates :shape, presence: true
   validates :duration, presence: true
@@ -9,4 +11,16 @@ class Sighting < ApplicationRecord
   validates :state, presence: true
   validates :longitude, presence: true
   validates :latitude, presence: true
+
+  # Sets default value for missing data
+  def init
+    self.datetime ||= 'N/A'
+    self.string ||= 'N/A'
+    self.integer ||= 'N/A'
+    self.text ||= 'N/A'
+    self.string ||= 'N/A'
+    self.string ||= 'N/A'
+    self.decimal ||= 'N/A'
+    self.decimal ||= 'N/A'
+  end
 end
