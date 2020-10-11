0e8bbb94b9fc877025e5fe2bcbb78b1bd79ffc26
Kaemon Lovendahl <klovendahl@deseretbook.com>
2020-10-11 16:33:44 -0600

    Remove allow_nil values

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,26 +1,12 @@
 # frozen_string_literal: true
 
 class Sighting < ApplicationRecord
-  after_initialize :init
-
-  validates :sighting_date, presence: true, allow_nil: true
+  validates :sighting_date, presence: true
   validates :shape, presence: true
-  validates :duration, presence: true, allow_nil: true
+  validates :duration, presence: true
   validates :comments, presence: true
   validates :city, presence: true
   validates :state, presence: true
-  validates :longitude, presence: true, allow_nil: true
-  validates :latitude, presence: true, allow_nil: true
-
-  # Sets default value for missing data
-  def init
-    self.sighting_date ||= nil
-    self.shape ||= 'N/A'
-    self.duration ||= nil
-    self.comments ||= 'N/A'
-    self.city ||= 'N/A'
-    self.state ||= 'N/A'
-    self.longitude ||= nil
-    self.latitude ||= nil
-  end
+  validates :longitude, presence: true
+  validates :latitude, presence: true
 end
