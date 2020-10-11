8ba45f91e4bd34d6f8b911cb95dacf2958d67b23
Kaemon Lovendahl <klovendahl@deseretbook.com>
2020-10-11 14:52:29 -0600

    Add simple validation tests for sighting

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class Sighting < ApplicationRecord
+  validates :sighting_date, presence: true
+  validates :shape, presence: true
+  validates :duration, presence: true
+  validates :comments, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
 end
--- test/models/sighting_test.rb
+++ test/models/sighting_test.rb
@@ -1,7 +1,62 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class SightingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @sighting = Sighting.new(
+      sighting_date: Date.new,
+      shape: 'oval',
+      duration: 5,
+      comments: 'It just hovered there.... MENACINGLY!',
+      city: 'South Jordan',
+      state: 'Utah',
+      longitude: 1.2345,
+      latitude: 5.678
+    )
+  end
+
+  test 'succeeds with all values present' do
+    assert @sighting.save
+  end
+
+  test 'fails without date' do
+    @sighting.sighting_date = nil
+    refute @sighting.save
+  end
+
+  test 'fails without shape' do
+    @sighting.shape = nil
+    refute @sighting.save
+  end
+
+  test 'fails without duration' do
+    @sighting.duration = nil
+    refute @sighting.save
+  end
+
+  test 'fails without comments' do
+    @sighting.comments = nil
+    refute @sighting.save
+  end
+
+  test 'fails without city' do
+    @sighting.city = nil
+    refute @sighting.save
+  end
+
+  test 'fails without state' do
+    @sighting.state = nil
+    refute @sighting.save
+  end
+
+  test 'fails without longitude' do
+    @sighting.longitude = nil
+    refute @sighting.save
+  end
+
+  test 'fails without latitude' do
+    @sighting.latitude = nil
+    refute @sighting.save
+  end
 end
