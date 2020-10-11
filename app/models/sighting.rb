cf70de5ba04f5f1a2b085ce2b84576b245f63fc4
Kaemon Lovendahl <klovendahl@deseretbook.com>
2020-10-11 16:08:51 -0600

    Fix mistake for default values

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -3,24 +3,24 @@
 class Sighting < ApplicationRecord
   after_initialize :init
 
-  validates :sighting_date, presence: true
+  validates :sighting_date, presence: true, allow_nil: true
   validates :shape, presence: true
-  validates :duration, presence: true
+  validates :duration, presence: true, allow_nil: true
   validates :comments, presence: true
   validates :city, presence: true
   validates :state, presence: true
-  validates :longitude, presence: true
-  validates :latitude, presence: true
+  validates :longitude, presence: true, allow_nil: true
+  validates :latitude, presence: true, allow_nil: true
 
   # Sets default value for missing data
   def init
-    self.datetime ||= 'N/A'
-    self.string ||= 'N/A'
-    self.integer ||= 'N/A'
-    self.text ||= 'N/A'
-    self.string ||= 'N/A'
-    self.string ||= 'N/A'
-    self.decimal ||= 'N/A'
-    self.decimal ||= 'N/A'
+    self.sighting_date ||= nil
+    self.shape ||= 'N/A'
+    self.duration ||= nil
+    self.comments ||= 'N/A'
+    self.city ||= 'N/A'
+    self.state ||= 'N/A'
+    self.longitude ||= nil
+    self.latitude ||= nil
   end
 end
