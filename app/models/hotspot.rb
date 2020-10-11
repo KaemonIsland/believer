83f7f1fec8fe0b830c43c17571abe3f83860bb90
Kaemon Lovendahl <klovendahl@deseretbook.com>
2020-10-11 14:39:01 -0600

    Add simple validation tests for hotspot

--- app/models/hotspot.rb
+++ app/models/hotspot.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Hotspot < ApplicationRecord
+  validates :name, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
 end
--- test/models/hotspot_test.rb
+++ test/models/hotspot_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class HotspotTest < ActiveSupport::TestCase
-  test 'the truth' do
-    assert true
+  test 'fails without name' do
+    hotspot = Hotspot.new(longitude: 1.2345, latitude: 5.678)
+    refute hotspot.save
+  end
+
+  test 'fails without longitude' do
+    hotspot = Hotspot.new(name: 'test', latitude: 5.678)
+    refute hotspot.save
+  end
+
+  test 'fails without latitude' do
+    hotspot = Hotspot.new(name: 'test', longitude: 1.2345)
+    refute hotspot.save
   end
 end
